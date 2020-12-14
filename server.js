9b68cf232faf7aa65ebdb35e95e63bc6e7ac3d3a
Andrea <andrea88@live.se>
2020-12-14 14:32:20 +0100

    done

--- server.js
+++ server.js
@@ -1,7 +1,53 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// Endpoint Author
+const Author = mongoose.model('Author', {
+  name: String
+})
+
+// Endpoint Book
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+})
+
+if (process.env.RESET_DATABASE) {
+  console.log('Resetting database')
+
+  const seedDatabase = async () => {
+    await Author.deleteMany() // To not keep on adding authors when updating code
+    await Book.deleteMany() 
+
+    const tolkien = new Author({ name: 'J.R.R Tolkien' })
+    await tolkien.save()
+
+    const rowling = new Author({ name: 'J.K. Rowling' })
+    await rowling.save()
+
+    await new Book({ title: "Harry Potter and the Philosopher's Stone", author: rowling }).save()
+    await new Book({ title: "Harry Potter and the Chamber of Secrets", author: rowling }).save()
+    await new Book({ title: "Harry Potter and the Prisoner of Azkaban", author: rowling }).save()
+    await new Book({ title: "Harry Potter and the Goblet of Fire", author: rowling }).save()
+    await new Book({ title: "Harry Potter and the Order of the Phoenix", author: rowling }).save()
+    await new Book({ title: "Harry Potter and the Half-Blood Prince", author: rowling }).save()
+    await new Book({ title: "Harry Potter and the Deathly Hallows", author: rowling }).save()
+    await new Book({ title: "The Lord of the Rings", author: tolkien }).save()
+    await new Book({ title: "The Hobbit", author: tolkien }).save()
+
+    console.log('hello Andrea')
+  }
+  seedDatabase()
+}
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -15,7 +61,40 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello Andrea')
+})
+
+// Route to get all authors
+app.get('/authors', async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
+})
+
+// Route to author id
+app.get('/authors/:id', async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  if(author) {
+    res.json(author)
+  } else {
+    res.status(404).json({ error: 'Author not found'})
+  }
+  })
+
+// Get collection of books from a author 
+app.get('/authors/:id/books', async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  if (author) {
+    const books = await Book.find({ author: mongoose.Types.ObjectId(author.id) })
+    res.json(books)
+  } else {
+    res.status(404).json({ error: 'Author not found'})
+  }
+})
+
+// Route to get Books
+app.get('/books', async (req, res) => {
+  const books = await Book.find().populate('author')
+  res.json(books)
 })
 
 // Start the server
